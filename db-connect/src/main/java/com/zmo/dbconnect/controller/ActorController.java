94489321a59968c16d244d843593ba1a5388480c
Zmo1914 <emil.dinev@hotmail.co.uk>
2022-07-28 23:42:21 +0300

    Create getActorById method and service.

--- db-connect/src/main/java/com/zmo/dbconnect/controller/ActorController.java
+++ db-connect/src/main/java/com/zmo/dbconnect/controller/ActorController.java
@@ -1,12 +1,16 @@
 package com.zmo.dbconnect.controller;
 
 import com.zmo.dbconnect.dto.ActorDto;
+import com.zmo.dbconnect.exception.RecordNotFoundException;
+import com.zmo.dbconnect.model.Actor;
 import com.zmo.dbconnect.repository.ActorRepository;
 import com.zmo.dbconnect.service.ActorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -23,11 +27,27 @@ public record ActorController(ActorService actorService) {
 
     @DeleteMapping("/{actor_id}")
     public ResponseEntity<Integer> deleteActor(@PathVariable("actor_id") Integer actor_id) {
-        boolean isDeleted = actorService.deleteActor(actor_id);
+        boolean isDeleted = actorService.deleteActorById(actor_id);
         if (!isDeleted) {
+            log.info("No actor with id={} found.", actor_id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping("/{actor_id}")
+    public ResponseEntity<Actor> getActorById(@PathVariable Integer actor_id) {
+        Optional<Actor> actor =  actorService.getActorById(actor_id);
+        if (actor.isPresent()){
+            return new ResponseEntity<>(actor.get(), HttpStatus.OK);
+        } else {
+            throw new RecordNotFoundException();
+        }
+
+
+
+
+    }
+
+
 }
--- db-connect/src/main/java/com/zmo/dbconnect/service/ActorService.java
+++ db-connect/src/main/java/com/zmo/dbconnect/service/ActorService.java
@@ -5,10 +5,13 @@ import com.zmo.dbconnect.model.Actor;
 import com.zmo.dbconnect.repository.ActorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -23,14 +26,36 @@ public record ActorService(ActorRepository actorRepository) {
         actorRepository.save(actor);
     }
 
-    public boolean deleteActor(Integer id) {
+    public boolean deleteActorById(Integer actor_id) {
         try {
-            actorRepository.deleteById(id);
+            actorRepository.deleteById(actor_id);
             return true;
-        }catch (EmptyResultDataAccessException e){
-            log.info("Cannot find Actor with ID={}",id);
-            log.info("Message: {}",e.getMessage());
+        } catch (EmptyResultDataAccessException e) {
+            log.info("Cannot find Actor with ID={}", actor_id);
+            log.info("Message: {}", e.getMessage());
             return false;
         }
+    }
+
+    public boolean deleteActors(List<Integer> actors_id) {
+        int deletedCounter = 0;
+        for (int i = 0; i < actors_id.size(); i++) {
+            try {
+                actorRepository.deleteById(actors_id.get(i));
+                deletedCounter++;
+            } catch (EmptyResultDataAccessException e) {
+                log.info("Cannot find Actor with ID={}", i);
+                log.info("Message: {}", e.getMessage());
+            }
+        }
+        if (deletedCounter > 0)
+            return true;
+        return false;
+    }
+
+
+    public Optional<Actor> getActorById(Integer actor_id) {
+        return Optional.ofNullable(actorRepository.findActorById(actor_id));
+
     }
 }
